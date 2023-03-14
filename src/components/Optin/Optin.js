d570e207b9fd88b7389ee37bdfe56912011f72fb
Ifera <iferareaves@gmail.com>
2023-03-15 00:59:46 +0500

    updates

---
+++ src/components/Optin/Optin.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Optin.css";
+
+class Optin extends Component {
+  render() {
+    return (
+      <div className="optin">
+        <p>Stay tuned. Our website will be launching soon!</p>
+      </div>
+    );
+  }
+}
+
+export default Optin;
